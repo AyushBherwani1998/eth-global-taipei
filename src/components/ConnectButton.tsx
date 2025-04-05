b92f283bcd540ce54a4371abb769cabde53fbbfa
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2025-04-05 10:06:55 +0800

    add landing page

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useConnect } from "wagmi";
+import { Button } from "@/components/Button";
 
 export default function ConnectButton() {
   const { connect, connectors } = useConnect();
@@ -8,13 +9,14 @@ export default function ConnectButton() {
   return (
     <div className="button-container">
       {connectors.map((connector) => (
-        <button
-          className="button"
+        <Button
           onClick={() => connect({ connector })}
           key={connector.id}
+          variant="outline"
+          className="bg-zinc-800 border-zinc-700 text-white"
         >
           Connect with {connector.name}
-        </button>
+        </Button>
       ))}
     </div>
   );
