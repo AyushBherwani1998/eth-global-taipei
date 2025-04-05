1e65a4ae333f999d80b1450e762fa4f00e7ddeff
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2025-04-05 10:35:39 +0800

    add home page

--- src/components/SelfQRCode.tsx
+++ src/components/SelfQRCode.tsx
@@ -3,15 +3,19 @@
 import { useSelfApp } from "@/hooks/useSelfApp";
 import SelfQRcodeWrapper from "@selfxyz/qrcode";
 
-export const SelfQrCode = () => {
+interface SelfQrCodeProps {
+  onSuccess: () => void;
+}
+
+export const SelfQrCode = ({ onSuccess }: SelfQrCodeProps) => {
   const { selfApp } = useSelfApp();
   return (
     <SelfQRcodeWrapper
       selfApp={selfApp}
-        onSuccess={() => {
-          console.log('Verification successful');
-          // Perform actions after successful verification
-        }}
+      onSuccess={() => {
+        onSuccess();
+        console.log("Verification successful");
+      }}
       />
     );
   }
